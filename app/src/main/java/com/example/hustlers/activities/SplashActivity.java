4914fa513a418220c9e5dda6b8c7971bbaf8b95e
Whiz Khotsoka <shanekhotsoka@gmail.com>
2021-08-28 13:58:18 -0700

    commit

--- app/src/main/java/com/example/hustlers/activities/SplashActivity.java
+++ app/src/main/java/com/example/hustlers/activities/SplashActivity.java
@@ -4,9 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.example.hustlers.R;
+import com.github.ybq.android.spinkit.sprite.Sprite;
+import com.github.ybq.android.spinkit.style.DoubleBounce;
+import com.github.ybq.android.spinkit.style.ThreeBounce;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -14,6 +18,10 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_activity);
+
+        ProgressBar progressBar = (ProgressBar)findViewById(R.id.spin_kit);
+        Sprite doubleBounce = new ThreeBounce();
+        progressBar.setIndeterminateDrawable(doubleBounce);
     }
 
     @Override
